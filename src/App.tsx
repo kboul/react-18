df3c6e722a785f139861e98e86d774ba851a620b
kboul <kos.voulgaridis@gmail.com>
2023-03-24 00:01:40 +0200

    Toggle Alert by clicking a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Container, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 import { Alert, Button, ListGroup } from "./components";
 
 export default function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
@@ -14,13 +17,12 @@ export default function App() {
           items={["New York", "San Francisco", "Tokyo", "London", "Paris"]}
           onSelectItem={handleSelectItem}
         />
-        <Alert>Hello World</Alert>
+        {showAlert && (
+          <Alert onAlertClick={() => setShowAlert(false)}>Hello World</Alert>
+        )}
 
-        <Button
-          colorScheme="blue"
-          onClick={() => console.log("button clicked")}
-        >
-          Button
+        <Button colorScheme="blue" onClick={() => setShowAlert(true)}>
+          Show alert
         </Button>
       </Stack>
     </Container>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,15 +1,19 @@
-import { Alert, AlertIcon } from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, CloseButton } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface AppAlertProps {
   children: ReactNode;
+  onAlertClick?: () => void;
 }
 
-export default function AppAlert({ children }: AppAlertProps) {
+export default function AppAlert({ children, onAlertClick }: AppAlertProps) {
   return (
     <Alert status="info">
       <AlertIcon />
       {children}
+      <Box flex={1} />
+
+      <CloseButton onClick={onAlertClick} />
     </Alert>
   );
 }
