3aa3b1dabc21944cfd8181d1efe9bfc45e38e13d
kboul <kos.voulgaridis@gmail.com>
2023-03-23 23:01:39 +0200

    Managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,30 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
 export default function ListGroup() {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>();
+
   const message = items.length === 0 && <p>No item found</p>;
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClick = (index: number) => () => setSelectedIndex(index);
 
   return (
     <>
       <h1>List</h1>
       {message}
       <ul>
-        {items.map((item) => (
-          <li key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            onClick={handleClick(index)}
+            style={{
+              cursor: "pointer",
+              backgroundColor:
+                selectedIndex === index ? "lightblue" : "transparent",
+              width: "15%",
+            }}
+          >
             {item}
           </li>
         ))}
