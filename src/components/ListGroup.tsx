6d296960f6afd6af910ea3df86f358ff305d7fd1
kboul <kos.voulgaridis@gmail.com>
2023-03-23 22:48:37 +0200

    Conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
-const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+items = [];
 
 export default function ListGroup() {
+  const message = items.length === 0 && <p>No item found</p>;
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul>
         {items.map((item) => (
           <li key={item}>{item}</li>
