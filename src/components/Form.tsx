1678b16a58120b0ccd4e9337c9d2f4267bcc4bc0
kboul <kos.voulgaridis@gmail.com>
2023-03-27 22:40:28 +0300

    Accessing input fields via refs on form submit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,27 @@
 import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 
 export default function Form() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submitted");
+    person.name = nameRef.current?.value || "";
+    person.age = parseInt(ageRef.current?.value || "0");
+    console.log(person);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <FormControl>
         <FormLabel>Name</FormLabel>
-        <Input />
+        <Input ref={nameRef} />
 
         <FormLabel mt={4}>Age</FormLabel>
-        <Input type="number" />
+        <Input type="number" ref={ageRef} />
       </FormControl>
       <Button mt={4} colorScheme="teal" type="submit">
         Submit
