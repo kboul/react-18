70b2982e2404561422474678f8da33ba410bbe98
kboul <kos.voulgaridis@gmail.com>
2023-03-28 22:40:07 +0300

    Validation using react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,24 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 export default function Form() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -10,7 +26,19 @@ export default function Form() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <FormLabel>Name</FormLabel>
-        <Input {...register("name")} />
+        <Input {...register("name", { required: true, minLength: 3 })} />
+        {errors.name?.type === "required" && (
+          <Alert status="error">
+            <AlertDescription>The name field is required</AlertDescription>
+          </Alert>
+        )}
+        {errors.name?.type === "minLength" && (
+          <Alert status="error">
+            <AlertDescription>
+              The name must be at least 3 characters
+            </AlertDescription>
+          </Alert>
+        )}
 
         <FormLabel mt={4}>Age</FormLabel>
         <Input {...register("age")} type="number" />
