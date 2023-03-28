f091d0d1a4bad37ef49f0057463fd3051e46c438
kboul <kos.voulgaridis@gmail.com>
2023-03-28 22:58:23 +0300

    Schema based validation with zod lib

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,18 +7,24 @@ import {
   Input,
 } from "@chakra-ui/react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "You must be at least 18 years old" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -26,22 +32,20 @@ export default function Form() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <FormLabel>Name</FormLabel>
-        <Input {...register("name", { required: true, minLength: 3 })} />
-        {errors.name?.type === "required" && (
+        <Input {...register("name")} />
+        {errors.name && (
           <Alert status="error">
-            <AlertDescription>The name field is required</AlertDescription>
-          </Alert>
-        )}
-        {errors.name?.type === "minLength" && (
-          <Alert status="error">
-            <AlertDescription>
-              The name must be at least 3 characters
-            </AlertDescription>
+            <AlertDescription>{errors.name.message}</AlertDescription>
           </Alert>
         )}
 
         <FormLabel mt={4}>Age</FormLabel>
-        <Input {...register("age")} type="number" />
+        <Input {...register("age", { valueAsNumber: true })} type="number" />
+        {errors.age && (
+          <Alert status="error">
+            <AlertDescription>{errors.age.message}</AlertDescription>
+          </Alert>
+        )}
       </FormControl>
       <Button mt={4} colorScheme="teal" type="submit">
         Submit
