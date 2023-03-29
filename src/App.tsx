4308aa3460dd5dc0c9d4ea1b8754b725e3d79d30
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2023-03-29 14:50:40 +0300

    Feature/building forms (#3)
    * Export Cities comp, create a Form component and handle the submit event
    
    * Accessing input fields via refs on form submit
    
    * Accessing input fields via useState hook as controlled components on form submit
    
    * Managing forms using react-hook-form lib
    
    * Validation using react-hook-form
    
    * Schema based validation with zod lib
    
    * Disable the submit button if form is invalid
    
    * Rename Form to PersonForm & comment out some components on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,32 @@
-import { Container, Flex, Stack } from "@chakra-ui/react";
-import { useState } from "react";
-import { FaCity } from "react-icons/fa";
+import { Container, Stack } from "@chakra-ui/react";
 
 import {
-  Alert,
-  Button,
-  Like,
-  ListGroup,
-  Player,
-  Pizza,
-  ShoppinCart,
+  // Like,
+  // Player,
+  // Pizza,
+  // ShoppinCart,
   ExpandableText,
+  // Cities,
+  PersonForm,
 } from "./components";
 
 export default function App() {
-  const [showAlert, setShowAlert] = useState(false);
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <Container>
       <Stack spacing={4}>
-        <ListGroup
-          heading={
-            <Flex>
-              Cities <FaCity size="20" />
-            </Flex>
-          }
-          items={["New York", "San Francisco", "Tokyo", "London", "Paris"]}
-          onSelectItem={handleSelectItem}
-        />
-        {showAlert && (
-          <Alert onAlertClick={() => setShowAlert(false)}>Hello World</Alert>
-        )}
+        {/* <Cities />
+        <hr /> */}
 
-        <Button
-          colorScheme="blue"
-          onClick={() => setShowAlert(true)}
-          width="28"
-        >
-          Show alert
-        </Button>
+        {/* <Like onClick={() => console.log("clicked")} /> */}
 
-        <hr />
+        {/* <hr />
+        <Player /> */}
 
-        <Like onClick={() => console.log("clicked")} />
-
-        <hr />
-        <Player />
-
-        <hr />
+        {/* <hr />
         <Pizza />
 
         <hr />
-        <ShoppinCart />
+        <ShoppinCart /> */}
 
         <hr />
         <ExpandableText maxChars={21}>
@@ -69,6 +40,9 @@ export default function App() {
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
         </ExpandableText>
+
+        <hr />
+        <PersonForm />
       </Stack>
     </Container>
   );
---
+++ src/components/PersonForm.tsx
@@ -0,0 +1,57 @@
+import {
+  Alert,
+  AlertDescription,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "You must be at least 18 years old" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  console.log(!isValid);
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input {...register("name")} />
+        {errors.name && (
+          <Alert status="error">
+            <AlertDescription>{errors.name.message}</AlertDescription>
+          </Alert>
+        )}
+
+        <FormLabel mt={4}>Age</FormLabel>
+        <Input {...register("age", { valueAsNumber: true })} type="number" />
+        {errors.age && (
+          <Alert status="error">
+            <AlertDescription>{errors.age.message}</AlertDescription>
+          </Alert>
+        )}
+      </FormControl>
+      <Button mt={4} colorScheme="teal" type="submit" isDisabled={!isValid}>
+        Submit
+      </Button>
+    </form>
+  );
+}
