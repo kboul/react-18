9e11a83f3c4bf286a45247ad392f3fdbcda6a7e4
kboul <kos.voulgaridis@gmail.com>
2023-03-30 22:29:54 +0300

    Cancelling a fetch request using abortController

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 import { Alert } from ".";
@@ -17,17 +17,25 @@ export default function Users() {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const getUsers = async () => {
       try {
         const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
+          "https://jsonplaceholder.typicode.com/users",
+          {
+            signal: controller.signal,
+          }
         );
         setUsers(res.data);
       } catch (err) {
+        if (err instanceof CanceledError) return;
         setError((err as AxiosError).message);
       }
     };
     getUsers();
+
+    return () => controller.abort();
   }, []);
 
   return (
